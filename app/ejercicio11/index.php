8b4a4b35e4b5566162a34cab9711d069ba3d5a10
SanglaHector <sanglahector@gmail.com>
2021-03-23 19:55:56 -0300

    t

--- app/ejercicio11/index.php
+++ app/ejercicio11/index.php
@@ -1,2 +1,20 @@
 <?php
-echo 'este es el ejercicio 11';
+/*
+Aplicación No 11 (Potencias de números)
+Mostrar por pantalla las primeras 4 potencias de los números del uno 1 al 4 (hacer una función
+que las calcule invocando la función pow).
+*/
+$mensaje ="";
+for($i = 1; $i ++ ; $i < 5)
+{
+    for($j = 1 ; $j++ ; $j < 5)
+    {
+        $mensaje = $mensaje.calcularPow($j,$i)."<br>";
+    }
+}
+echo $mensaje;
+
+function calcularPow($base, $exponente)
+{
+    return pow($base, $exponente);
+}
